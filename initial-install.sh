a42819e2760f31923993085c9524ea2769bd61f9
Tyler Cash <contact@tylercash.xyz>
2016-10-03 18:19:51 +1100

    updates pacman information before getting packages

--- initial-install.sh
+++ initial-install.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+#updates system's information
+sudo pacman -Syu
+
+#Install needed depencies for config files in script
+sudo pacman -S bash-completion
+
+#REQUIRES bash-completion
 cp .bashrc ~/
