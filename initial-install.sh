0a5c6a920f169b759f581d7384368e8842b2cfea
Tyler Cash <contact@tylercash.xyz>
2016-11-29 17:10:10 +1100

    Added line to make bash case insensitive

--- initial-install.sh
+++ initial-install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#Enables case insensitive auto complete
+# If ~./inputrc doesn't exist yet, first include the original /etc/inputrc so we don't override it
+if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' > ~/.inputrc; fi
+
+# Add option to ~/.inputrc to enable case-insensitive tab completion
+echo 'set completion-ignore-case On' >> ~/.inputrc
+
 #updates system's information
 sudo apt update
 
