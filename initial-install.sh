7183d5c716a91e4203bee962d09b537f9a281a09
Tyler Cash <contact@tylercash.xyz>
2016-12-02 01:11:30 +1100

    Added .profile that auto decrypts home folder on login

--- initial-install.sh
+++ initial-install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#Enables case insensitive auto complete
-# If ~./inputrc doesn't exist yet, first include the original /etc/inputrc so we don't override it
-if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' > ~/.inputrc; fi
-
 # Add option to ~/.inputrc to enable case-insensitive tab completion
 echo 'set completion-ignore-case On' >> ~/.inputrc
 
@@ -16,3 +12,8 @@ sudo apt install bash-completion
 #REQUIRES bash-completion
 cp user/.bashrc ~/
 cp user/.gitconfig ~/
+cp user/.profile ~/
+
+#Enables case insensitive auto complete
+# If ~./inputrc doesn't exist yet, first include the original /etc/inputrc so we don't override it
+if [ ! -a ~/.inputrc ]; then echo '$include /etc/inputrc' > ~/.inputrc; fi
