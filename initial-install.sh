2a06cd0a5eb08ce4283cb6b6e302801d02f2e9d3
Tyler Cash <contact@tylercash.xyz>
2017-04-28 14:07:26 +0000

    Added auto daily update of system and packages via cronjobs

--- initial-install.sh
+++ initial-install.sh
@@ -22,3 +22,5 @@ wget https://gist.githubusercontent.com/Tyler-cash/9f61e8778dd69e7df87e7cfba8c91
 mkdir ~/.ssh
 cat public_key >> ~/.ssh/authorized_keys
 rm public_key
+cat update_tasks.sh >> /etc/cron.daily/updates
+chmod 755 /etc/cron.daily/updates
